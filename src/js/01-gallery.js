180488a5776d61c5679197bd01a8eb2204e03346
Alex Andrieiev <andryeyevalex@gmail.com>
2022-08-20 14:35:28 +0300

    Add markup of Task 1 and install SimpleLightBox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+console.log(galleryList);
+const galleryMarkup = createGalleryMarkup(galleryItems);
+console.log(galleryMarkup);
+
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+       <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+       </a>
+    `;
+    })
+    .join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a');
+gallery.options.captionsData = 'alt';
+gallery.options.captionDelay = '250';
+gallery.options.captionPosition = 'bottom';
